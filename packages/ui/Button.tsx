0724df2c3a8d9b3532054b836153d0447e2fe0e0
Charles Opute Odili <chaluwa@gmail.com>
2023-05-04 21:52:35 +0000

    feat(create-turbo): apply official-starter transform

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,9 +1,5 @@
 import * as React from "react";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
-}
-
-export const Button = ({ children, ...rest }: ButtonProps) => {
-  return <button {...rest}>{children}</button>;
+export const Button = () => {
+  return <button>Boop</button>;
 };
